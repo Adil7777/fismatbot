56d15fc6a1c5aab43bdc4c65dd54680ea79912b3
Adil7777 <ismagambetov,adil2005@gmail.com>
2020-06-08 17:20:43 +0600

    did 7 grade 8 grade and fmpt commands

--- bot.py
+++ bot.py
@@ -1,27 +1,43 @@
 from aiogram import Bot, Dispatcher, executor, types
 import config
 import logging
+import messages
 
 logging.basicConfig(level=logging.INFO)
 
 bot = Bot(token=config.TOKEN)
 dp = Dispatcher(bot)
 
-
-# @dp.message_handler()
-# async def echo(message: types.Message):
-#     await message.answer(message.text)
+"""Sending information to user about admission to grade 7"""
 
 
-@dp.message_handler(commands=['7'])
+@dp.message_handler(commands=['7_grade'])
 async def seven_grade(message: types.Message):
-    await message.answer("7 класс")
+    await message.answer(messages.SEVEN_GRADE_1)
+    await message.answer(messages.SEVEN_GRADE_2)
 
 
-@dp.message_handler(commands=['8'])
+"""Sending information to user about admission to grade 7"""
+
+
+@dp.message_handler(commands=['8_grade'])
 async def eight_grade(message: types.Message):
-    await message.answer("8 класс")
+    await message.answer(messages.EIGHT_GRADE)
 
 
+@dp.message_handler(commands=['fmpt'])
+async def eight_grade(message: types.Message):
+    await message.answer(messages.FMPT)
+
+
+"""If user don't know what write to bot, we send him this message"""
+
+
+@dp.message_handler()
+async def echo(message: types.Message):
+    await message.answer(messages.BASIC)
+
+
+"""If everything is ok, we start our program"""
 if __name__ == "__main__":
     executor.start_polling(dp, skip_updates=True)
