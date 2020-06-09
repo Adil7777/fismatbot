d7fa587ff2a7e4f6510051fe5fafe8e128f36e1a
Adil7777 <ismagambetov,adil2005@gmail.com>
2020-06-09 16:21:05 +0600

    funcs for selection committee ready, added endowment fund

--- second.py
+++ second.py
@@ -7,69 +7,99 @@ bot = telebot.TeleBot(config.TOKEN)
 
 """keybord"""
 
+back_button = types.KeyboardButton(text='Назад')
 
-@bot.message_handler(commands=['start'])
-def start(message):
+
+def main(id, text):
     keyboard_main = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)
     admission = types.KeyboardButton(text='👨‍🎓 Поступление')
     fmpt = types.KeyboardButton(text='🤓 FMPT')
     pee = types.KeyboardButton(text='📝 Основной вступительный экзамен')  # pee - primary entrance exam
     about = types.KeyboardButton(text='🏫 О РФМШ')
-    keyboard_main.add(admission, fmpt, pee, about)
-    bot.send_message(message.chat.id, 'Что вы хотите узнать', reply_markup=keyboard_main)
+    fund = types.KeyboardButton(text='💳 Fizmat Endowment Fund')
+    keyboard_main.add(admission, fmpt, pee, about, fund)
+    bot.send_message(id, text, reply_markup=keyboard_main)
+
+
+def fund(id, text):
+    keybord_fund = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)
+    what_is_it = types.KeyboardButton(text='🤷‍♂️Что такое? 🤷‍♀')
+    better = types.KeyboardButton(text='В чем же преимущество')
+    who_can_be = types.KeyboardButton(text='👨 Кто может стать участником 👩')
+    support = types.KeyboardButton(text='💹 Сделать вклад')
+    keybord_fund.add(what_is_it, better, who_can_be, support, back_button)
+    bot.send_message(id, text, reply_markup=keybord_fund)
+
+
+def exam(id, text):
+    keyboard_primery_entrance_exam = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)
+    seven = types.KeyboardButton(text='7 класс')
+    eight = types.KeyboardButton(text='8 класс')
+    keyboard_primery_entrance_exam.add(seven, eight, back_button)
+    bot.send_message(id, text, reply_markup=keyboard_primery_entrance_exam)
+
+
+def admission_(id, text):
+    keyboard_admission = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)
+    seven = types.KeyboardButton(text='Поступление в 7 класс')
+    eight = types.KeyboardButton(text='Поступление в 8 класс')
+    keyboard_admission.add(seven, eight, back_button)
+    bot.send_message(id, text, reply_markup=keyboard_admission)
+
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    main(message.chat.id, 'Что вы хотите узнать')
 
 
 @bot.message_handler(content_types=['text'])
 def msg(message):
-    def back(message):
-        keyboard_main = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)
-        admission = types.KeyboardButton(text='👨‍🎓 Поступление')
-        fmpt = types.KeyboardButton(text='🤓 FMPT')
-        pee = types.KeyboardButton(text='📝 Основной вступительный экзамен')  # pee - primary entrance exam
-        about = types.KeyboardButton(text='🏫 О РФМШ')
-        keyboard_main.add(admission, fmpt, pee, about)
-        bot.send_message(message.chat.id, 'Что вы хотите узнать', reply_markup=keyboard_main)
-
-    back_button = types.KeyboardButton(text='Назад')
-
     user_message = message.text
+    id = message.chat.id
     if user_message not in user_messages:
-        keyboard_main = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)
-        admission = types.KeyboardButton(text='👨‍🎓 Поступление')
-        fmpt = types.KeyboardButton(text='🤓 FMPT')
-        pee = types.KeyboardButton(text='📝 Основной вступительный экзамен')  # pee - primary entrance exam
-        about = types.KeyboardButton(text='🏫 О РФМШ')
-        keyboard_main.add(admission, fmpt, pee, about)
-        bot.send_message(message.chat.id, 'Извините, Ваш запрос не понятен. Пожалуйста, сформулируйте иначе?',
-                         reply_markup=keyboard_main)
+        main(id, 'Извините, Ваш запрос не понятен. Пожалуйста, сформулируйте иначе?')
 
     else:
         if user_message == '👨‍🎓 Поступление':
-            keyboard_admission = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)
-            seven = types.KeyboardButton(text='Поступление в 7 класс')
-            eight = types.KeyboardButton(text='Поступление в 8 класс')
-            keyboard_admission.add(seven, eight, back_button)
-            bot.send_message(message.chat.id, '👨‍🎓 Поступление', reply_markup=keyboard_admission)
+            admission_(id, '👨‍🎓 Поступление')
+
         elif user_message == '🤓 FMPT':
-            keyboard_fmpt = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)
-            keyboard_fmpt.add(back_button)
-            bot.send_message(message.chat.id, FMPT, reply_markup=keyboard_fmpt)
+            main(id, FMPT)
+
         elif user_message == '📝 Основной вступительный экзамен':
-            keyboard_primery_entrnce_exam = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)
-            seven = types.KeyboardButton(text='7 класс')
-            eight = types.KeyboardButton(text='8 класс')
-            keyboard_primery_entrnce_exam.add(seven, eight, back_button)
-            bot.send_message(message.chat.id, '📝 Основной вступительный экзамен',
-                             reply_markup=keyboard_primery_entrnce_exam)
+            exam(id, '📝 Основной вступительный экзамен')
+
+        elif user_message == '💳 Fizmat Endowment Fund':
+            fund(id, '💳 Fizmat Endowment Fund')
+
         elif user_message == 'Поступление в 7 класс':
-            admission_seven = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)
-            admission_seven.add(back_button)
-            bot.send_message(message.chat.id, '{}\n{}'.format(SEVEN_GRADE_1, SEVEN_GRADE_2),
-                             reply_markup=admission_seven)
+            admission_(id, '{}\n{}'.format(SEVEN_GRADE_1, SEVEN_GRADE_2))
+
+        elif user_message == 'Поступление в 8 класс':
+            admission_(id, EIGHT_GRADE)
+
+        elif user_message == '7 класс':
+            exam(id, PEE_7)
+
+        elif user_message == '8 класс':
+            exam(id, PEE_8)
+
+        elif user_message == '🤷‍♂️Что такое? 🤷‍♀':
+            fund(id, WHAT_IS_IT)
+
+        elif user_message == 'В чем же преимущество':
+            fund(id, BETTER)
+
+        elif user_message == '👨 Кто может стать участником 👩':
+            fund(id, WHO_CAN_BE)
+
+        elif user_message == '💹 Сделать вклад':
+            fund(id, DONATE)
+
         elif user_message == 'Назад':
-            back(message)
+            main(id, 'Что вы хотите узнать')
 
 
 if __name__ == "__main__":
-    print('eee')
+    print('program starting')
     bot.polling(none_stop=True)
